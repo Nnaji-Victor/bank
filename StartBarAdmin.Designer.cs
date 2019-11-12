d172b85b961960221d76f5bdeb4092f9214ecdb7
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-12 12:54:20 +0100

    add startbar admin loading screen

---
+++ StartBarAdmin.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Banking_System
+{
+    partial class prgStartAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.prgBarAdmin = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // prgBarAdmin
+            // 
+            this.prgBarAdmin.Location = new System.Drawing.Point(200, 192);
+            this.prgBarAdmin.Name = "prgBarAdmin";
+            this.prgBarAdmin.Size = new System.Drawing.Size(343, 37);
+            this.prgBarAdmin.TabIndex = 0;
+            this.prgBarAdmin.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // prgStartAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prgBarAdmin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "prgStartAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.startBar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgBarAdmin;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ StartBarAdmin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking_System
+{
+    public partial class prgStartAdmin : Form
+    {
+        public prgStartAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startBar_Load(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            prgBarAdmin.Value = prgBarAdmin.Value + 1;
+
+            if (prgBarAdmin.Value >= 99)
+            {
+                Admin a = new Admin();
+                this.Hide();
+                a.Show();
+
+                timer1.Enabled = false;
+                prgBarAdmin.Value -= 1;
+            }
+        }
+    }
+}
