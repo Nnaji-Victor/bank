cd0fbee367697b66390699f703e5b7362d1a34a1
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-12 12:57:54 +0100

    add withdraw for customers

---
+++ withdraw.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Banking_System
+{
+    partial class withdraw
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAccntType = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.txtdate = new System.Windows.Forms.DateTimePicker();
+            this.txtTotalBalance = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWithdraw = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCustId = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAccntType);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnWithdraw);
+            this.groupBox1.Controls.Add(this.txtdate);
+            this.groupBox1.Controls.Add(this.txtTotalBalance);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtWithdraw);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtCustId);
+            this.groupBox1.Location = new System.Drawing.Point(45, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(798, 615);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cash Withdrawal";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtAccntType
+            // 
+            this.txtAccntType.AutoSize = true;
+            this.txtAccntType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAccntType.Location = new System.Drawing.Point(391, 356);
+            this.txtAccntType.Name = "txtAccntType";
+            this.txtAccntType.Size = new System.Drawing.Size(85, 25);
+            this.txtAccntType.TabIndex = 22;
+            this.txtAccntType.Text = "Deposit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(125, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 25);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Account Type";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(396, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 57);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnWithdraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWithdraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWithdraw.Location = new System.Drawing.Point(137, 523);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(152, 57);
+            this.btnWithdraw.TabIndex = 19;
+            this.btnWithdraw.Text = "Withdraw";
+            this.btnWithdraw.UseVisualStyleBackColor = false;
+            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
+            // 
+            // txtdate
+            // 
+            this.txtdate.Location = new System.Drawing.Point(396, 284);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(224, 22);
+            this.txtdate.TabIndex = 18;
+            // 
+            // txtTotalBalance
+            // 
+            this.txtTotalBalance.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotalBalance.Enabled = false;
+            this.txtTotalBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalBalance.Location = new System.Drawing.Point(411, 46);
+            this.txtTotalBalance.Multiline = true;
+            this.txtTotalBalance.Name = "txtTotalBalance";
+            this.txtTotalBalance.Size = new System.Drawing.Size(224, 33);
+            this.txtTotalBalance.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(125, 418);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Withdraw:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 36);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Total Balance:";
+            // 
+            // txtWithdraw
+            // 
+            this.txtWithdraw.Location = new System.Drawing.Point(396, 410);
+            this.txtWithdraw.Multiline = true;
+            this.txtWithdraw.Name = "txtWithdraw";
+            this.txtWithdraw.Size = new System.Drawing.Size(224, 33);
+            this.txtWithdraw.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Date";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(110, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(179, 25);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Account Number:";
+            // 
+            // txtCustId
+            // 
+            this.txtCustId.Enabled = false;
+            this.txtCustId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustId.Location = new System.Drawing.Point(396, 176);
+            this.txtCustId.Multiline = true;
+            this.txtCustId.Name = "txtCustId";
+            this.txtCustId.Size = new System.Drawing.Size(224, 33);
+            this.txtCustId.TabIndex = 0;
+            // 
+            // withdraw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 714);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "withdraw";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "withdraw";
+            this.Load += new System.EventHandler(this.withdraw_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label txtAccntType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.DateTimePicker txtdate;
+        private System.Windows.Forms.TextBox txtTotalBalance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWithdraw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCustId;
+    }
+}
---
+++ withdraw.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking_System
+{
+    public partial class withdraw : Form
+    {
+        public withdraw()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(AccountCreation.conString);
+        public static string balance;
+
+        private void withdraw_Load(object sender, EventArgs e)
+        {
+            
+            con.Open();
+            string query = "select accountId,balance,accountType from Account where customerId=@loggedin";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlParameter val;
+            val = new SqlParameter();
+            val.SqlDbType = SqlDbType.VarChar;
+            cmd.Parameters.AddWithValue("@loggedin", frmLogin.getLoggedInUser());
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                txtCustId.Text = dr[0].ToString();
+                balance = dr[1].ToString();
+                txtTotalBalance.Text = "₦" + balance;
+                txtAccntType.Text = "Big boys "+ dr[2].ToString() + " Account";
+            }
+
+            con.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+            string accNum, date;
+            double bal, withdraw;
+
+            accNum = txtCustId.Text;
+            date = txtdate.Text;
+            bal = double.Parse(balance);
+            withdraw = double.Parse(txtWithdraw.Text);
+
+            con.Open();
+            SqlCommand command = con.CreateCommand();
+            SqlTransaction transaction;
+
+            transaction = con.BeginTransaction();
+            command.Connection = con;
+            command.Transaction = transaction;
+
+            try
+            {
+                command.Parameters.AddWithValue("@accNum", accNum);
+                command.Parameters.AddWithValue("@date", date);
+                command.Parameters.AddWithValue("@bal", bal);
+                command.Parameters.AddWithValue("@withdraw", withdraw);
+
+                command.CommandText = "INSERT INTO Transactions(AccountId,date,balance,withdraw) VALUES(@accNum,@date,@bal,@withdraw)";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "update [Account] set balance = balance - @deposit where accountId=@accNum";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+                MessageBox.Show("Transaction Successful!!!");
+                string query = "select accountId,balance from [Account] where customerId=@user";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@user", frmLogin.getLoggedInUser());
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txtCustId.Text = dr[0].ToString();
+                    balance = dr[1].ToString();
+                    txtTotalBalance.Text = "₦" + balance;
+                }
+                txtWithdraw.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
