261c8b71811214f4bb62070b791624930d56bbb8
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-12 12:53:41 +0100

    add deposit form and code

---
+++ Deposit.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Banking_System
+{
+    partial class Deposit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCustId = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDeposit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotalBalance = new System.Windows.Forms.TextBox();
+            this.txtdate = new System.Windows.Forms.DateTimePicker();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnDeposit);
+            this.groupBox1.Controls.Add(this.txtdate);
+            this.groupBox1.Controls.Add(this.txtTotalBalance);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDeposit);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtCustId);
+            this.groupBox1.Location = new System.Drawing.Point(27, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(798, 615);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cash Deposit";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtCustId
+            // 
+            this.txtCustId.Enabled = false;
+            this.txtCustId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustId.Location = new System.Drawing.Point(396, 176);
+            this.txtCustId.Multiline = true;
+            this.txtCustId.Name = "txtCustId";
+            this.txtCustId.Size = new System.Drawing.Size(224, 33);
+            this.txtCustId.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(110, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(179, 25);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Account Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Date";
+            // 
+            // txtDeposit
+            // 
+            this.txtDeposit.Location = new System.Drawing.Point(396, 410);
+            this.txtDeposit.Multiline = true;
+            this.txtDeposit.Name = "txtDeposit";
+            this.txtDeposit.Size = new System.Drawing.Size(224, 33);
+            this.txtDeposit.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 36);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Total Balance:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(125, 418);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Deposit";
+            // 
+            // txtTotalBalance
+            // 
+            this.txtTotalBalance.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotalBalance.Enabled = false;
+            this.txtTotalBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalBalance.Location = new System.Drawing.Point(411, 46);
+            this.txtTotalBalance.Multiline = true;
+            this.txtTotalBalance.Name = "txtTotalBalance";
+            this.txtTotalBalance.Size = new System.Drawing.Size(224, 33);
+            this.txtTotalBalance.TabIndex = 17;
+            // 
+            // txtdate
+            // 
+            this.txtdate.Location = new System.Drawing.Point(396, 284);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(224, 22);
+            this.txtdate.TabIndex = 18;
+            // 
+            // btnDeposit
+            // 
+            this.btnDeposit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDeposit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeposit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeposit.Location = new System.Drawing.Point(137, 523);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(152, 57);
+            this.btnDeposit.TabIndex = 19;
+            this.btnDeposit.Text = "Deposit";
+            this.btnDeposit.UseVisualStyleBackColor = false;
+            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(396, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 57);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Deposit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 670);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Deposit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Deposit";
+            this.Load += new System.EventHandler(this.Deposit_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCustId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.DateTimePicker txtdate;
+        private System.Windows.Forms.TextBox txtTotalBalance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDeposit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ Deposit.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking_System
+{
+    public partial class Deposit : Form
+    {
+        public Deposit()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(AccountCreation.conString);
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string balance;
+        private void Deposit_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "select accountId,balance from [Account] where customerId=@user";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@user", frmLogin.getLoggedInUser());
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                txtCustId.Text = dr[0].ToString();
+                balance = dr[1].ToString();
+                txtTotalBalance.Text = "₦"+balance;
+            }
+
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDeposit_Click(object sender, EventArgs e)
+        {
+            string accNum, date;
+            double bal, deposit;
+
+            accNum = txtCustId.Text;
+            date = txtdate.Text;
+            bal = double.Parse(balance);
+            deposit = double.Parse(txtDeposit.Text);
+
+            con.Open();
+            SqlCommand command = con.CreateCommand();
+            SqlTransaction transaction;
+
+            transaction = con.BeginTransaction();
+            command.Connection = con;
+            command.Transaction = transaction;
+
+            try
+            {
+                command.Parameters.AddWithValue("@accNum", accNum);
+                command.Parameters.AddWithValue("@date", date);
+                command.Parameters.AddWithValue("@bal", bal);
+                command.Parameters.AddWithValue("@deposit", deposit);
+                
+
+                command.CommandText = "INSERT INTO Transactions(AccountId,date,balance,deposit) VALUES(@accNum,@date,@bal,@deposit)";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "update [Account] set balance = balance +@deposit where accountId=@accNum";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+                MessageBox.Show("Transaction Successful!!!");
+                string query = "select accountId,balance from [Account] where customerId=@user";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@user", frmLogin.getLoggedInUser());
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txtCustId.Text = dr[0].ToString();
+                    balance = dr[1].ToString();
+                    txtTotalBalance.Text = "₦" + balance;
+                }
+                txtDeposit.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
