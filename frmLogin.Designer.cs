7540a8069756683823602dad60c338ae68e9206a
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-12 12:52:54 +0100

    add login form and code

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Banking_System
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLoadStatus = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(512, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 584);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Tomato;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(339, 424);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(165, 53);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(113, 424);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(165, 53);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(268, 251);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(261, 44);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(268, 153);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(261, 43);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome To DecaBank";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(738, 750);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Visible = false;
+            // 
+            // txtLoadStatus
+            // 
+            this.txtLoadStatus.AutoSize = true;
+            this.txtLoadStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLoadStatus.Location = new System.Drawing.Point(697, 740);
+            this.txtLoadStatus.Name = "txtLoadStatus";
+            this.txtLoadStatus.Size = new System.Drawing.Size(64, 25);
+            this.txtLoadStatus.TabIndex = 2;
+            this.txtLoadStatus.Text = "label5";
+            this.txtLoadStatus.Visible = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 4000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.txtLoadStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DecaBank";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label txtLoadStatus;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ frmLogin.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking_System
+{
+    public partial class frmLogin : Form
+    {
+
+        public static string username;
+        public frmLogin()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static string getLoggedInUser()
+        {
+            return username;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            var a = AccountCreation.conString;
+            SqlConnection con = new SqlConnection(a);
+           
+
+            if (username.StartsWith("ADM",StringComparison.OrdinalIgnoreCase))
+            {
+                con.Open();
+                string query = "Select * from [Admin] where username='" + username + "' and password='" + password + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                DataTable dtb = new DataTable();
+
+                sda.Fill(dtb);
+                if (dtb.Rows.Count == 1)
+                {
+                    txtLoadStatus.ForeColor = Color.Green;
+                    txtLoadStatus.Visible = true;
+                    txtLoadStatus.Text = "Login succesful!....";
+                    prgStartAdmin sba = new prgStartAdmin();
+                    this.Hide();
+                    sba.Show();
+                }
+
+                else
+                {
+                    txtLoadStatus.Text = "Invalid Username or Password";
+                }
+                con.Close();
+            }
+            else if(username.StartsWith("VIC",StringComparison.OrdinalIgnoreCase))
+            {
+                con.Open();
+                string query = "Select * from [Account] where customerId='" + username + "' and password='" + password + "'";
+
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                DataTable dtb = new DataTable();
+
+                sda.Fill(dtb);
+                if (dtb.Rows.Count == 1)
+                {
+                    getLoggedInUser();
+                    txtLoadStatus.Text = "Login succesful!....";
+                    prgStart sb = new prgStart();
+                    this.Hide();
+                    sb.Show();
+                }
+
+
+                else
+                {
+                    txtLoadStatus.Text = "Invalid Username or Password";
+                }
+
+                con.Close();
+            }
+            else if ( username == "" && password == "")
+            {
+                txtLoadStatus.Text = "Type in your username and password";
+                txtLoadStatus.ForeColor = Color.Red;
+                txtLoadStatus.Visible = true;
+            }
+            else
+            {
+                txtLoadStatus.Text = "Incorrect username and or password";
+                txtLoadStatus.ForeColor = Color.Red;
+                txtLoadStatus.Visible = true;
+                
+            }
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            txtLoadStatus.Text = "";
+        }
+    }
+}
