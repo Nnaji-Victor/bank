ab3334bcce76e12807f1bdd922d1b180458b65f6
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-12 12:57:38 +0100

    add startbar for customers

---
+++ startBar.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Banking_System
+{
+    partial class prgStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.prgBar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // prgBar
+            // 
+            this.prgBar.Location = new System.Drawing.Point(199, 174);
+            this.prgBar.Name = "prgBar";
+            this.prgBar.Size = new System.Drawing.Size(343, 37);
+            this.prgBar.TabIndex = 0;
+            this.prgBar.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // prgStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prgBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "prgStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.startBar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgBar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ startBar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banking_System
+{
+    public partial class prgStart : Form
+    {
+        public prgStart()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startBar_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            prgBar.Value = prgBar.Value + 1;
+
+            if(prgBar.Value >= 99)
+            {
+                Customer c = new Customer();
+                this.Hide();
+                c.Show();
+
+                timer1.Enabled = false;
+                prgBar.Value = prgBar.Value - 1;
+            }
+        }
+    }
+}
